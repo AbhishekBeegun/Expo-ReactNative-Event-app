32bf53a11b3f056fb8e4e553006aaca796aec0d4
Dooshyant Beegun <abhishekbeegun0642@gmail.com>
2023-03-21 16:59:01 +0400

    paramsadded

--- app/artist-details/[id].js
+++ app/artist-details/[id].js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useRouter,useSearchParams } from "expo-router"
-import { Text,View } from "react-native"
+import { Text,View,SafeAreaView,ScrollView } from "react-native"
 import Header from "../../components/Artists/components/Header"
 import About from "../../components/Artists/components/About"
 import Socials from "../../components/Artists/components/Socials"
 import Display from "../../components/Artists/components/Display"
+import Footer from "../../components/Basiclayout/Footer"
 
 const ArtistDetails = () => {
   const params = useSearchParams()
   const router = useRouter()
   return (
+    <SafeAreaView>
+      <ScrollView>
     <View>
         <Text>
           {params.id}
@@ -23,6 +26,11 @@ const ArtistDetails = () => {
           <Display/>  
         </View>
     </View>
+
+
+    <Footer/>
+    </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- app/event-details/[slug].js
+++ app/event-details/[slug].js
@@ -1,17 +1,29 @@
 import React from 'react'
-import { Text,View } from "react-native"
+import { useRouter,useSearchParams } from "expo-router"
+import { Text,View,SafeAreaView,ScrollView } from "react-native"
 import EventHeader from "../../components/Events/components/EventHeader"
 import EventAbout from "../../components/Events/components/EventAbout"
 import EventSocials from "../../components/Events/components/EventSocials"
 import EventDisplay from "../../components/Events/components/EventDisplay"
+import Footer from "../../components/Basiclayout/Footer"
 
 
 
 
 const EventDetails = () => {
+
+  const params = useSearchParams()
+  const router = useRouter()
+
   return (
+    <SafeAreaView>
+      <ScrollView>
+
+
     <View>
         <Text>
+        {params.slug}
+
             Event Details
         </Text>
 
@@ -22,6 +34,11 @@ const EventDetails = () => {
           <EventDisplay/>          
         </View>
     </View>
+
+    <Footer/>
+
+    </ScrollView>
+    </SafeAreaView>
   )
 }
 
